7ca33b50e84a3bcd89d6d2fcc61e0e31821b2a0a
H.M.E <2827011270@qq.com>
2021-05-05 11:08:33 +0800

    perfomance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+    <city-alphabet :cities="cities" @change="handLetterChange"></city-alphabet>
   </div>
 </template>
 <script>
@@ -31,12 +31,18 @@ export default {
         this.cities = data.cities;
         this.hotCities = data.hotCities;
       }
+    },
+    handLetterChange(letter) {
+      // console.log(letter);
+      this.letter = letter;
     }
   },
+
   data() {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ""
     };
   },
   mounted() {
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -32,6 +32,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+// 样式穿透，不受scoped控制了
 .wrapper >>> .swiper-pagination-bullet-active
     background:#fff
 .wrapper
@@ -39,7 +40,7 @@ export default {
     width:100%
     height:0
     padding-bottom :31.25%
-    backgoud  :#eee
+    background:#eee
     .swiper-img
         width:100%
 </style>
